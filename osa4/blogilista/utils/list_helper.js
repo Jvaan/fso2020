c3f077a1f0437c7c2de879122e035485e54b95e9
Pykälämäki <juha.pykalamaki@kavokerr.com>
2020-06-02 18:12:41 +0300

    4.5-4.7: apufunktioita ja yksikkötestejä, steps 3-5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,6 +1,62 @@
+const _ = require('lodash')
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
+}
+
+const authorWithMostBlogs = blogs => {
+  const reducer = (previousMax, currentAuthor) => {
+    const newBlogObject = {
+      author: _.head(currentAuthor).author,
+      blogs: currentAuthor.length
+    }
+    return (previousMax.blogs > newBlogObject.blogs) ? previousMax : newBlogObject
+  }
+
+  if (blogs.length === 0) {
+    return {}
+  } else {
+    const groupByAuthor = _.groupBy(blogs, 'author') // get collection which is grouped by author
+    const authorBlogs = _.reduce(groupByAuthor, reducer, 0) // get one author with most blogs
+    return authorBlogs
+  }
+}
+
+const mostLikes = blogs => {
+  const reducer = (previousMax, currentAuthor) => {
+    const newBlogObject = {
+      author: _.head(currentAuthor).author,
+      likes: _.sumBy(currentAuthor, (author) => { return author.likes })
+    }
+    return (previousMax.likes > newBlogObject.likes) ? previousMax : newBlogObject
+  }
+
+  if (blogs.length === 0) {
+    return {}
+  } else {
+    const groupByAuthor = _.groupBy(blogs, 'author') // get collection which is grouped by author
+    const authorBlogs = _.reduce(groupByAuthor, reducer, 0) // get one author with most likes
+    return authorBlogs
+  }
+}
+
+const blogWithMostLikes = blogs => {
+  const reducer = (previousMax, currentBlog) => {
+    return (previousMax.likes > currentBlog.likes) ? previousMax : currentBlog
+  }
+  if (blogs.length === 0) {
+    return {}
+  } else {
+    const blog = blogs.reduce(reducer, 0)
+
+    const newBlogObject = {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes
+    }
+    return newBlogObject
+  }
 }
 
 const totalLikes = blogs => {
@@ -13,5 +69,5 @@ const totalLikes = blogs => {
 }
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, blogWithMostLikes, authorWithMostBlogs, mostLikes
 }
